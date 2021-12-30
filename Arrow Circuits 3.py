a59dae507ec315cd223eda7e3bdd616245260e37
Max <gamepiatnitza@gmail.com>
2021-12-30 12:03:52 +0200

    Grid logical array

--- Arrow Circuits 3.py
+++ Arrow Circuits 3.py
@@ -5,9 +5,9 @@ tk = Tk()
 tk['bg'] = '#ffffff'
 tk.title('Arrow Circuits')
 
-emptyCell = PhotoImage(name="Empty cell", file="resources/empty_cell.png")
+emptyCell = PhotoImage(name="Eraser", file="resources/empty_cell.png")
 electrifiedEmptyCell = PhotoImage(name="Electrify", file="resources/electrified_empty_cell.png")
-arrowPointingUp = PhotoImage("Arrow", file="resources/arrow_pUP.png")
+arrowPointingUp = PhotoImage(name="Arrow", file="resources/arrow_pUP.png")
 arrowPointingDown = PhotoImage(file="resources/arrow_pDOWN.png")
 arrowPointingLeft = PhotoImage(file="resources/arrow_pLEFT.png")
 arrowPointingRight = PhotoImage(file="resources/arrow_pRIGHT.png")
@@ -34,17 +34,29 @@ canvas = Canvas(tk, height = window_height, width = window_width)
 canvas.pack()
 
 #GRID
-grid = ()
+grid = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
 
-for y in range (0, window_height - img_height * 3, img_height):
-    for x in range(0, window_width, img_width):
-        canvas.create_image(x, y, anchor = NW, image = emptyCell)
+for i in range (0, int((window_height - img_height * 3) / 57), 1):
+    for j in range(0, window_width, img_width):
+        grid[i].append(emptyCell)
 
+def draw_image(imgNumber, image):
+    canvas.create_image(imgNumber[0] * img_width, imgNumber[1] * img_height, anchor = NW, image = image)
+
+def get_img_number(x, y):
+    return (int(x / img_width), int(y / img_height))
+
+def draw_grid(grid):
+    for iy in range (0, len(grid)):
+        for ix in range(0, len(grid[iy])):
+            draw_image((ix, iy), grid[iy][ix])
+
+draw_grid(grid)
 #MENU
 def select_tool(tool):
     global selected_image
     selected_image = tool
-    print("[LOG]: selected tool: " + tool.name)
+    print("Selected tool: " + tool.name)
 
 menuButtonElectrify = Button(tk, image = menuIconElectrify, command = lambda: select_tool(electrifiedEmptyCell))
 menuButtonElectrify.place(x=50, y= img_height * 15.5)
@@ -52,16 +64,13 @@ menuButtonElectrify.place(x=50, y= img_height * 15.5)
 menuButtonArrow = Button(tk, image = menuIconArrow, command = lambda: select_tool(arrowPointingUp))
 menuButtonArrow.place(x=214, y= img_height * 15.5)
 
-def draw_image(imgNumber, image):
-    canvas.create_image(imgNumber[0] * img_width, imgNumber[1] * img_height, anchor = NW, image = image)
-
 def change_image(imgNumber, image):
     draw_image(imgNumber, image)
     
 def fix_image(event):
     if event.widget != canvas:
         return
-    imgNumber = (int(event.x / img_width), int(event.y / img_height))
+    imgNumber = get_img_number(event.x, event.y)
     if imgNumber[0] < 33 and imgNumber[1] < 15:
         draw_image(imgNumber, selected_image)
 
