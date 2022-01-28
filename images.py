afc9da55d184e67149e66e7850454d461fda7f13
Max <gamepiatnitza@gmail.com>
2022-01-28 07:41:07 +0200

    Moved variables with PhotoImages initialization to the individual file

---
+++ images.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+#ICON
+appIcon = PhotoImage(name="icon", file="resources/arrow_circuits_icon.ico")
+
+#CELLS
+emptyCell = PhotoImage(name="Eraser", file="resources/empty_cell.png")
+generator = PhotoImage(name="Create generator", file="resources/generator.png")
+arrowPointingUp = PhotoImage(name="Arrow", file="resources/arrow_pUP.png")
+arrowPointingDown = PhotoImage(name="a_d", file="resources/arrow_pDOWN.png")
+arrowPointingLeft = PhotoImage(name="a_l", file="resources/arrow_pLEFT.png")
+arrowPointingRight = PhotoImage(name="a_r", file="resources/arrow_pRIGHT.png")
+electrifiedArrowPointingUp = PhotoImage(name = "ea_u", file="resources/electrified_arrow_pUP.png")
+electrifiedArrowPointingDown = PhotoImage(name = "ea_d", file="resources/electrified_arrow_pDOWN.png")
+electrifiedArrowPointingLeft = PhotoImage(name = "ea_l", file="resources/electrified_arrow_pLEFT.png")
+electrifiedArrowPointingRight = PhotoImage(name = "ea_r", file="resources/electrified_arrow_pRIGHT.png")
+
+#MENU ICONS
+menuIconArrow = PhotoImage(file="resources/arrow_menu_icon.png")
+menuIconCreateGenerator = PhotoImage(file="resources/create_generator_menu_icon.png")
+menuIconEraser = PhotoImage(file="resources/eraser_menu_icon.png")
+menuIconRotator = PhotoImage(file="resources/rotator_menu_icon.png")
